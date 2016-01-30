e22d9739b6dc6ec61cabbba83513062ade0969d3
westtrumpet <west.trumpet@gmail.com>
2016-01-30 00:27:26 -0600

    Initial commit. Models, Player Customizer fields, scene setup

---
+++ GlobalGameJam/Assets/playerCustom.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerCustom : MonoBehaviour {
+
+	public Material[] colors;
+	public GameObject[] models;
+	public GameObject[] stands;
+	public Transform modelPos;
+	public Transform standPos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
